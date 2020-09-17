24ab48c422d919d2afc1881698abdb970f0be344
cieloazul310 <cieloazul310@gmail.com>
2020-09-17 16:20:52 +0900

    update

--- plugins/gatsby-plugin-top-layout/components/AppStateProvider.tsx
+++ plugins/gatsby-plugin-top-layout/components/AppStateProvider.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import AppStateContext from '../../../src/utils/AppStateContext';
 import reducer, { useInitialAppState } from '../../../src/utils/AppState';
 
